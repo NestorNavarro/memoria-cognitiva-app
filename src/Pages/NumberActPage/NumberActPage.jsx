a14220b6ecae704629add3660e698404fe3cd691
NestorNavarro <nestor.navarroarizga@gmail.com>
2021-10-04 23:09:07 -0500

    number memory started

--- src/Pages/NumberActPage/NumberActPage.jsx
+++ src/Pages/NumberActPage/NumberActPage.jsx
@@ -1,11 +1,52 @@
-import React from "react";
+import { TextField, Box } from "@mui/material";
+import LinearProgress from '@mui/material/LinearProgress';
 
-const NumberActPage = () => {
+import "./styles.css"
+
+const NumberActPage = ({
+    delegations : {
+        answer,
+        progress,
+        isLoading,
+        setAnswer,
+        randomNumber,
+        handleSubmit,
+    }
+}) => {
+
     return (
-        <div>
-            Recuerda el numero
-        </div>
+        <form 
+            className="container"
+            onSubmit={handleSubmit}
+        >
+            <div className="card card-content">
+                { isLoading &&
+                    <>
+                        <h1 className="number">{randomNumber}</h1>
+                        <Box className="mt-5" sx={{ width: '50%' }}>
+                            <LinearProgress variant="determinate" value={progress} />
+                        </Box>
+                    </>
+                }
+                { !isLoading && 
+                    <>
+                        <h1>Ingresa el número</h1>
+                        <TextField
+                            autoFocus={true}
+                            hiddenLabel
+                            type="number"
+                            variant="filled"
+                            value={answer}
+                            onChange={({ target }) => setAnswer(target.value)}
+                            sx={{
+                                width : "250px",
+                            }}
+                        />
+                    </>
+                }
+
+            </div>
+        </form>
     );
 }
-
 export default NumberActPage;
--- src/Pages/NumberActPage/NumberActPageContainer.jsx
+++ src/Pages/NumberActPage/NumberActPageContainer.jsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NumberActPage from "./NumberActPage.jsx";
 
 const NumberActPageContainer = () => {
+    const [progress, setProgress]         = useState(0);
+    const [answer, setAnswer]             = useState("");
+    const [ronunds, setRounds]            = useState(1);
+    const [isLoading, setIsLoading]       = useState(true);
+    const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * 9));
+
+    const getRandomNumberByLevel = (min, max) => {
+        return Math.floor(Math.random() * (max - min) + min)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(Number(answer), randomNumber)
+        if (Number(answer) === randomNumber) {
+            console.log("correct");
+        } else {
+            console.log("incorrect");
+        }
+        setAnswer("");
+        const {min, max} = getNumber();
+        setRandomNumber(getRandomNumberByLevel(min, max));
+        setIsLoading(true);
+    }
+
+    const getNumber = () => {
+        let maxNumber = "1";
+        let minNumber = "1";
+        for (let i=0;i<ronunds;i++) {
+            maxNumber += "0";
+            if(i + 1 < ronunds) {
+                minNumber += "0";
+            }
+        }
+        return {
+            min : Number(minNumber),
+            max : Number(maxNumber)
+        };
+    }
+    useEffect(() => {
+        if (isLoading) {
+            setRounds((value) => value + 1);
+            const timer = setInterval(() => {
+              setProgress((oldProgress) => {
+                if (oldProgress === 100) {
+                    setIsLoading(false);
+                    return 0;
+                }
+                return oldProgress + 20;
+              });   
+            }, 500);
+            
+                return () => {
+                  clearInterval(timer);
+                };
+        }
+      }, [isLoading]);
     return (
-        <NumberActPage />
+        <NumberActPage
+            delegations={{
+                answer,
+                progress,
+                isLoading,
+                setAnswer,
+                randomNumber,
+                handleSubmit,
+            }}
+        />
     );
 }
 export default NumberActPageContainer;
