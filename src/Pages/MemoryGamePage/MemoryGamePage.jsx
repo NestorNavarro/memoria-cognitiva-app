9e9d81cfa779dc2dfefe54f193b5e0cda2b2f134
NestorNavarro <nestor.navarroarizga@gmail.com>
2021-10-27 23:12:42 -0500

    memo title fixed

--- src/Pages/MemoryGamePage/MemoryGamePage.jsx
+++ src/Pages/MemoryGamePage/MemoryGamePage.jsx
@@ -1,43 +1,46 @@
-import { Button }   from "@mui/material";
+import { Button } from "@mui/material";
 
 // Our Importations
-import GameOver             from "../../Components/GameOver";
-import DashBoardActivity    from "../../Components/DashBoardActivity";
-import Board                from "../../Components/Board/Board";
-import "./styles.css"
+import GameOver from "../../Components/GameOver";
+import DashBoardActivity from "../../Components/DashBoardActivity";
+import Board from "../../Components/Board/Board";
+import "./styles.css";
 
-const testName    = "Memorama"
-const instructions = "Relaciona las imágenes."
+const testName = "Memorama";
+const instructions = "Relaciona las imágenes.";
 
 const MemoryGamePage = ({
-    delegations:{
+    delegations: {
         attemps,
         animating,
         haveCards,
         startGame,
         setStartGame,
         handleMemoClick,
-        shuffeldMemoBlocks
-    }
+        shuffeldMemoBlocks,
+    },
 }) => {
     return (
         <>
-            {!startGame ?
-            <DashBoardActivity activity={testName}  instructions={instructions} setStartGame={setStartGame}/>
-            :(haveCards ?
-                    <>
-                        <Board memoBlocks={shuffeldMemoBlocks} animating={animating} handleMemoClick={handleMemoClick}/>
-                        <div className = "Title">
-                            Errores : {attemps - 1}
-                        </div>
-                    </>
-                    :
-                    <GameOver
-                    score = {attemps}
-                    route = "/dashboard/memory_game" />
-                )
-            }
+            {!startGame ? (
+                <DashBoardActivity
+                    activity={testName}
+                    instructions={instructions}
+                    setStartGame={setStartGame}
+                />
+            ) : haveCards ? (
+                <>
+                    <h1 className="title">Errores : {attemps - 1}</h1>
+                    <Board
+                        memoBlocks={shuffeldMemoBlocks}
+                        animating={animating}
+                        handleMemoClick={handleMemoClick}
+                    />
+                </>
+            ) : (
+                <GameOver score={attemps} route="/dashboard/memory_game" />
+            )}
         </>
     );
-}
+};
 export default MemoryGamePage;
